714c10d3d0e38059ed8bea9f1c7ba4090782f1c1
AKSHIT <132344370+22f3000604@users.noreply.github.com>
2025-06-11 13:56:17 +0000

    added login page

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,url_for,request,render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -13,7 +15,22 @@ def index():
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.session.query(User).filter_by(name=username).first()
+        if user and check_password_hash(user.password, password):
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html', error='Invalid credentials')
+        
+    return render_template('signup.html')    
+
+
+
+        
+        
 
 if __name__ ==  "__main__":
     app.run(debug = True,port = 8080)
